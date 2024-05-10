eb32c2f764d897458dd4bbab12e53d9ceeb318c0
omar344 <3marabadi@gmail.com>
2024-05-10 03:02:56 +0300

    To Update a Profile

---
+++ Data/Migrations/20240509174827_AddApplicationUser.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                schema: "security",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                schema: "security",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                schema: "security",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: true,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                schema: "security",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                schema: "security",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                schema: "security",
+                table: "Users");
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Diagnostics.CodeAnalysis;
+
+namespace UserManagmentWithIdentity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [AllowNull]
+        public byte[] ProfilePicture { get; set; }
+    }
+}
